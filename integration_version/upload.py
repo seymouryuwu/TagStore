e06e1f12be9233bc0332e5ef82663acc64358004
YuWu-Seymour <ywuu0108@student.monash.edu>
2020-06-13 19:07:17 +1000

    Update upload.py

--- integration_version/upload.py
+++ integration_version/upload.py
@@ -1,6 +1,7 @@
 import json
 import base64
 import boto3
+import random
 
 s3 = boto3.client("s3")
 
@@ -8,11 +9,13 @@ def lambda_handler(event, context):
     if event['httpMethod'] == 'POST' :
         data = json.loads(event['body'])
         name = data['name']
+        random_number = '{:016}'.format(random.randrange(1, 10**16))
+        image_name = random_number + name
 
         image_data = data['image'].encode('utf-8')
         image = base64.b64decode(image_data)
         
-        s3_upload = s3.put_object(Bucket="s3fit5225", Key=name, Body=image, ACL='public-read')
+        s3_upload = s3.put_object(Bucket="s3fit5225", Key=image_name, Body=image, ACL='public-read')
     
     return {
         'statusCode': 200,
