ed5c626d40255da8bf02fe7f6133a158b557c87e
YuWu-Seymour <ywuu0108@student.monash.edu>
2020-06-14 12:03:00 +1000

    Update upload.py

--- integration_version/upload.py
+++ integration_version/upload.py
@@ -19,5 +19,11 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps("Upload successfully.")
+        'body': json.dumps("Upload successfully."),
+        'headers': {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Credentials" : "true",
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+        }
     }
